d0fe114ad0dc790e4e0185a491de662b52e1afed
CalculusIII <hnguy9@uic.edu>
2021-03-09 21:34:45 +0700

    doing somthing with index

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const portHome = 3000;
+//const portNews = 1001;
+//const portDetail = 1010;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Homepage <br />'+
+  '<a href="/uwu" onclick=>click me</a> <br />' +
+  '<a href="/test" onclick=>testing</a> <br />');
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/uwu', (req, res) => {
+    res.send(' <a href="/" onclick=>click me</a>');
+})
+
+app.get('/test', function(req, res) {
+    res.sendFile('/index.html', {root: __dirname });
+    //res.send(' <a href="/" onclick=>click me</a>');
+});
+
+app.listen(portHome, () => {
+  console.log(`http://localhost:${portHome}`);
+  console.log('using \\news to go to news page');
 })
