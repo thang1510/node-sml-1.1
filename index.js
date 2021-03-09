2dd0df7cc582e27b0beecf696f2f0d7c26997ca0
CalculusIII <hnguy9@uic.edu>
2021-03-09 20:17:41 +0700

    hello word index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello master UwU');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  res.send('Hi master, UwU')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
